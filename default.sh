8e13e49eecf8b5710939ac63aa3a854bbcb4cd1e
Rendai09 <118589549+Rendai09@users.noreply.github.com>
2025-09-28 13:04:34 +0700

    Update default.sh

--- default.sh
+++ default.sh
@@ -14,7 +14,7 @@ EXTENSIONS=(
     "https://github.com/Bing-su/adetailer"
     "https://github.com/zanllp/sd-webui-infinite-image-browsing"
     "https://github.com/hako-mikan/sd-webui-regional-prompter"
-    "https://github.com/TheSlateGray/sd-civitai-browser-plus"
+    "https://github.com/SignalFlagZ/sd-webui-civbrowser"
     "https://github.com/ArtVentureX/sd-webui-agent-scheduler"
     "https://github.com/thomasasfk/sd-webui-aspect-ratio-helper"
     "https://github.com/hako-mikan/sd-webui-supermerger"
