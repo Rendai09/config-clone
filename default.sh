687672fee7e57445a63ec38e6ae783ad19c256b9
Rendai09 <118589549+Rendai09@users.noreply.github.com>
2025-12-04 10:56:47 +0700

    Update default.sh

--- default.sh
+++ default.sh
@@ -32,7 +32,7 @@ CHECKPOINT_MODELS=(
     "https://huggingface.co/Rendai/RandeiTheWitchModel/resolve/main/StellarRINGV2.1_BAKED.safetensors"
     #"https://huggingface.co/datasets/Rendai/CloneData/resolve/main/ILTest/illustriousXL20_v20.safetensors"
     #"https://huggingface.co/Rendai/RandeiTheWitchModel/resolve/main/CrucibleRING.safetensors"
-    "https://huggingface.co/Rendai/RandeiTheWitchModel/resolve/main/CrucibleRINGv2.3.fp16.safetensors"
+    #"https://huggingface.co/Rendai/RandeiTheWitchModel/resolve/main/CrucibleRINGv2.3.fp16.safetensors"
     #"https://huggingface.co/Rendai/RandeiTheWitchModel/resolve/main/crucibleRINGSDXL_v2.8.safetensors"
 )
 
