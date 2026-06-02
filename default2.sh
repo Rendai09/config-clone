4f43c27fdbd31f275b2b8f50702618c4bce26d12
Rendai09 <118589549+Rendai09@users.noreply.github.com>
2026-06-02 16:35:42 +0700

    Update default2.sh

--- default2.sh
+++ default2.sh
@@ -49,7 +49,7 @@ CHECKPOINT_MODELS=(
     #"https://huggingface.co/circlestone-labs/Anima/resolve/main/split_files/diffusion_models/anima-preview3-base.safetensors"
     #"https://civitai.red/api/download/models/2843829?type=Model&format=SafeTensor&size=pruned&fp=bf16"
     "https://huggingface.co/FallenIncursio/PaSanctuary/resolve/main/PaSanctuary_v5.safetensors"
-    "https://huggingface.co/hollowstrawberry/holymix/resolve/main/HolyMix_illustriousXL_v1.safetensors"
+    #"https://huggingface.co/hollowstrawberry/holymix/resolve/main/HolyMix_illustriousXL_v1.safetensors"
     "https://huggingface.co/richyrich515/richyrichMixIXL/resolve/main/richyrichMixIXL-v1.fp16.safetensors"
 )
 
