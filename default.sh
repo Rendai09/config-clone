e9964226a749f145b872e0a47759795dbd01f8f9
Rendai09 <118589549+Rendai09@users.noreply.github.com>
2026-08-07 21:55:53 +0700

    Update default.sh

--- default.sh
+++ default.sh
@@ -46,12 +46,12 @@ CHECKPOINT_MODELS=(
     #"https://huggingface.co/FallenIncursio/Skirkscendance/resolve/main/Skirkscendance_v1.safetensors"
     #"https://huggingface.co/Manityro/Vermilion-Anima/resolve/main/Vermilion-0.1-AnimaV1.safetensors"
     #"https://huggingface.co/Manityro/Hoseki_LustrousMix_AnimaBaseV1_v1/resolve/main/Hoseki_LustrousMix_animaBaseV1_v1.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/sweetBapsRimixStylized_animaV10.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/milfSoup_v2Anima.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/oneObsessionAnima_v30.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/oneObsessionBranch_matureAnimaV1.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/rdbtAnime_v2Base.safetensors"
-    "https://huggingface.co/Rendai/ClondeModel/resolve/main/MCheckpoint/Holiskice3.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/sweetBapsRimixStylized_animaV10.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/milfSoup_v2Anima.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/oneObsessionAnima_v30.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/oneObsessionBranch_matureAnimaV1.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/rdbtAnime_v2Base.safetensors"
+    "https://huggingface.co/Rendai/ClondeModel/resolve/main/Holiskice3.safetensors"
 )
 
 UNET_MODELS=(
